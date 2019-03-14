2243bed1791afde21744ca9fe40d1d871d12e1cf
wlh <568674236@qq.com>
2019-03-14 14:45:40 +0800

    修改

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 $(document).ready(function() {
+    //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
+    $(function () {
+        $(".post-listing").scroll(function(){
+            if ($(".post-listing").scrollTop()>100){
+                $("#toTop").fadeIn(1500);
+            }
+            else
+            {
+                $("#toTop").fadeOut(1500);
+            }
+        });
+
+        //当点击跳转链接后，回到页面顶部位置
+        $("#toTop").click(function(){
+            //$('body,html').animate({scrollTop:0},1000);
+            if ($(".post-listing").scrollTop()) {
+                $(".post-listing").animate({ scrollTop: 0 }, 1000);
+                return false;
+            }
+            $(".post-listing").animate({ scrollTop: 0 }, 1000);
+            return false;
+        });
+    });
 });
