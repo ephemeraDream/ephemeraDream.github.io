cdd2aca60e02eea4b59fec7054181a4e6e214073
wlh <568674236@qq.com>
2019-03-14 16:36:42 +0800

    修改

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    var document;
-    if(window.innerWidth < 1170){
-        document = $("html");
-    }else {
-        document = $(".post-listing");
-    }
     //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
     $(function () {
+        var document;
+        if(window.innerWidth < 1170){
+            document = $("html");
+        }else {
+            document = $(".post-listing");
+        }
         document.scroll(function(){
             if (document.scrollTop()>100){
                 $("#toTop").fadeIn(500);
