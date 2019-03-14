6f9ff469694825e9acaf046ffb01c6e19c432d0c
wlh <568674236@qq.com>
2019-03-14 15:02:35 +0800

    修改

--- js/main.js
+++ js/main.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
+    var document;
+    if(window.innerWidth < 1170){
+        document = $("html");
+    }else {
+        document = $(".post-listing");
+    }
     //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
     $(function () {
-        $(".post-listing").scroll(function(){
-            if ($(".post-listing").scrollTop()>100){
-                $("#toTop").fadeIn(1500);
+        document.scroll(function(){
+            if (document.scrollTop()>100){
+                $("#toTop").fadeIn(500);
             }
             else
             {
-                $("#toTop").fadeOut(1500);
+                $("#toTop").fadeOut(500);
             }
         });
 
         //当点击跳转链接后，回到页面顶部位置
         $("#toTop").click(function(){
             //$('body,html').animate({scrollTop:0},1000);
-            if ($(".post-listing").scrollTop()) {
-                $(".post-listing").animate({ scrollTop: 0 }, 1000);
+            if (document.scrollTop()) {
+                document.animate({ scrollTop: 0 }, 1000);
                 return false;
             }
-            $(".post-listing").animate({ scrollTop: 0 }, 1000);
+            document.animate({ scrollTop: 0 }, 1000);
             return false;
         });
     });
