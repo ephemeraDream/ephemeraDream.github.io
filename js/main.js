2ce89b1ae1b252f9ee6c60e0a8ae6cb12f1f0946
wlh <568674236@qq.com>
2019-03-14 17:00:19 +0800

    修改

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
-    var document;
-    if(window.innerWidth < 1170){
-        document = $("html,body");
-    }else {
-        document = $(".post-listing");
-    }
     //当滚动条的位置处于距顶部100像素以下时，跳转链接出现，否则消失
     $(function () {
-        document.scroll(function(){
-            if (document.scrollTop()>100){
+        $(".post-listing").scroll(function(){
+            if ($(".post-listing").scrollTop()>100){
                 $("#toTop").fadeIn(500);
             }
             else
@@ -20,11 +14,11 @@ $(document).ready(function() {
         //当点击跳转链接后，回到页面顶部位置
         $("#toTop").click(function(){
             //$('body,html').animate({scrollTop:0},1000);
-            if (document.scrollTop()) {
-                document.animate({ scrollTop: 0 }, 1000);
+            if ($(".post-listing").scrollTop()) {
+                $(".post-listing").animate({ scrollTop: 0 }, 1000);
                 return false;
             }
-            document.animate({ scrollTop: 0 }, 1000);
+            $(".post-listing").animate({ scrollTop: 0 }, 1000);
             return false;
         });
     });
